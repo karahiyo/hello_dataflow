57f475822dd9e934e36950a26e7449c6c6a5049b
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-12-17 16:53:33 +0900

    ..

--- src/pubsub_stream_to_bigquery.py
+++ src/pubsub_stream_to_bigquery.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import logging
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions, StandardOptions
 
 
 def run(argv=None):
@@ -25,20 +25,23 @@ def run(argv=None):
             )
 
     known_args, pipeline_args = parser.parse_known_args(argv)
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    pipeline_options.view_as(StandardOptions).streaming = True
 
-    with beam.Pipeline(argv=pipeline_args) as p:
-        (p 
-                | "Read input from PubSub" >>
-                beam.io.gcp.pubsub.ReadFromPubSub(subscription=known_args.subscription) 
-                | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
-                | "Parse json" >> beam.Map(json.loads)
-                | "File load to BigQuery" >> beam.io.gcp.bigquery.WriteToBigQuery(
+    with beam.Pipeline(options=pipeline_options) as p:
+        (p
+         | "Read input from PubSub" >>
+         beam.io.gcp.pubsub.ReadFromPubSub(subscription=known_args.subscription)
+         | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
+         | "Parse json" >> beam.Map(json.loads)
+         | "File load to BigQuery" >> beam.io.gcp.bigquery.WriteToBigQuery(
                     table=known_args.output_table,
                     method=beam.io.WriteToBigQuery.Method.FILE_LOADS,
                     triggering_frequency=10,
                     write_disposition=beam.io.gcp.bigquery.BigQueryDisposition.WRITE_APPEND,
                     create_disposition=beam.io.gcp.bigquery.BigQueryDisposition.CREATE_NEVER)
-                )
+         )
 
 
 if __name__ == '__main__':
