d33c58e9c555664f634215eddf205a0f705f67ca
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-12-28 11:24:33 +0900

    add example - streaming in/out with custom metrics on Dataflow

---
+++ streaming_in_out_with_custom_metrics.py
@@ -0,0 +1,35 @@
+import argparse
+
+import apache_beam as beam
+from apache_beam.metrics import Metrics
+from apache_beam.options.pipeline_options import SetupOptions, PipelineOptions, StandardOptions
+
+
+class WordLengthMonitorDoFn(beam.DoFn):
+    def __init__(self):
+        self.words_length_monitor = Metrics.distribution(self.__class__, 'length')
+
+    def process(self, element: str, *args, **kwargs):
+        self.words_length_monitor.update(len(element))
+        return element
+
+
+def run(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_topic')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    pipeline_options.view_as(StandardOptions).streaming = True
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        messages = (p
+                    | beam.io.ReadFromPubSub(topic=known_args.input_topic).with_output_types(bytes)
+                    | 'Decode' >> beam.Map(lambda x: x.decode('utf-8')))
+
+        output = (
+                messages
+                | '_Monitoring' >> beam.ParDo(WordLengthMonitorDoFn())
+        )
+
