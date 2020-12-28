f58f074e968f70f4e2b855ed67c21616054d0f0c
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-12-28 11:03:58 +0900

    ..

--- hello_dataflow/examples/wordcount_with_metrics.py
+++ hello_dataflow/examples/wordcount_with_metrics.py
@@ -1,37 +1,39 @@
+import re
+
 import apache_beam as beam
 from apache_beam.metrics import Metrics
 
 
 class WordExtractingDoFn(beam.DoFn):
-  """Parse each line of input text into words."""
-  def __init__(self):
-    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
-    # beam.DoFn.__init__(self)
-    super(WordExtractingDoFn, self).__init__()
-    self.words_counter = Metrics.counter(self.__class__, 'words')
-    self.word_lengths_counter = Metrics.counter(self.__class__, 'word_lengths')
-    self.word_lengths_dist = Metrics.distribution(
-        self.__class__, 'word_len_dist')
-    self.empty_line_counter = Metrics.counter(self.__class__, 'empty_lines')
+    """Parse each line of input text into words."""
 
-  def process(self, element):
-    """Returns an iterator over the words of this element.
+    def __init__(self):
+        # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
+        # beam.DoFn.__init__(self)
+        super(WordExtractingDoFn, self).__init__()
+        self.words_counter = Metrics.counter(self.__class__, 'words')
+        self.word_lengths_counter = Metrics.counter(self.__class__, 'word_lengths')
+        self.word_lengths_dist = Metrics.distribution(
+            self.__class__, 'word_len_dist')
+        self.empty_line_counter = Metrics.counter(self.__class__, 'empty_lines')
 
-    The element is a line of text.  If the line is blank, note that, too.
+    def process(self, element):
+        """Returns an iterator over the words of this element.
 
-    Args:
-      element: the element being processed
+        The element is a line of text.  If the line is blank, note that, too.
 
-    Returns:
-      The processed element.
-    """
-    text_line = element.strip()
-    if not text_line:
-      self.empty_line_counter.inc(1)
-    words = re.findall(r'[\w\']+', text_line, re.UNICODE)
-    for w in words:
-      self.words_counter.inc()
-      self.word_lengths_counter.inc(len(w))
-      self.word_lengths_dist.update(len(w))
-    return words
+        Args:
+          element: the element being processed
 
+        Returns:
+          The processed element.
+        """
+        text_line = element.strip()
+        if not text_line:
+            self.empty_line_counter.inc(1)
+        words = re.findall(r'[\w\']+', text_line, re.UNICODE)
+        for w in words:
+            self.words_counter.inc()
+            self.word_lengths_counter.inc(len(w))
+            self.word_lengths_dist.update(len(w))
+        return words
